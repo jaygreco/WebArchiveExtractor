ea9aaa4ac5fe0e814ad84defb7e21095823a53dc
Nate Weaver <wevah@derailer.org>
2013-07-23 14:53:06 -0500

    Remove irritating NSLog. (Sorry.)

--- WebArchiveController.m
+++ WebArchiveController.m
@@ -11,7 +11,6 @@
 @implementation WebArchiveController
 
 - (void)awakeFromNib {
-	NSLog(@"%@", NSLocalizedStringFromTable(@"NSHumanReadableCopyright", @"InfoPlist", @"Copyright"));
 	[mainWindow setDelegate:self];
 }
 
